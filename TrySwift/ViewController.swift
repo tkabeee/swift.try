2f2d05203144accb82d88e801920a424ea639893
tkabeee <tkabee@gmail.com>
2016-02-12 09:53:17 +0900

    ボタンとテキストフィールドのアクションに対する処理を実装

--- TrySwift/ViewController.swift
+++ TrySwift/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBAction func tapHandler(sender: AnyObject) {
+    myTextField.text = "変更しました";
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBOutlet weak var myTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
 
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
 }
 
