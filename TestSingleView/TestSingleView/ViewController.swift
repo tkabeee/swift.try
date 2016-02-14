f52e32973bc58297b86ff7b5cc432fc3fa94f6ec
tkabeee <tkabee@gmail.com>
2016-02-14 16:50:21 +0900

    ボタンタップの際にテキストフィールドにテキストを代入

--- TestSingleView/TestSingleView/ViewController.swift
+++ TestSingleView/TestSingleView/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBAction func tapHandler(sender: AnyObject) {
+    myTextField.text = "タップしたよね？";
+  }
+
+  @IBOutlet weak var myTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
