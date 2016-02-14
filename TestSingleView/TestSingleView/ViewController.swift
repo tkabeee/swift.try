de1909e6fc5635fa71826b4cc7374ff1efa139df
tkabeee <tkabee@gmail.com>
2016-02-14 16:32:14 +0900

    シングルビューアプリケーション作成

---
+++ TestSingleView/TestSingleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSingleView
+//
+//  Created by Takayuki Kondo on 2/14/16.
+//  Copyright © 2016 tkabeee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
